cd002663cbaf106593e58df4830934944c1913ef
Matt Porritt <mattp@catalyst-au.net>
2017-01-13 14:09:45 +1100

    Add initial unit test setup and boiler plate

--- tests/engine_test.php
+++ tests/engine_test.php
@@ -25,3 +25,75 @@
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
+require_once($CFG->dirroot . '/search/tests/fixtures/testable_core_search.php');
+require_once($CFG->dirroot . '/search/tests/fixtures/mock_search_area.php');
+require_once($CFG->dirroot . '/search/engine/elastic/tests/fixtures/testable_engine.php');
+
+class search_elastic_engine_testcase extends advanced_testcase {
+    /**
+     * @var \core_search::manager
+     */
+    protected $search = null;
+
+    /**
+     * @var Instace of core_search_generator.
+     */
+    protected $generator = null;
+
+    /**
+     * @var Instace of testable_engine.
+     */
+    protected $engine = null;
+
+    public function setUp() {
+        $this->resetAfterTest();
+        set_config('enableglobalsearch', true);
+
+        if (!defined('TEST_SEARCH_ELASTIC_HOSTNAME') ||
+                !defined('TEST_SEARCH_ELASTIC_PORT') ||
+                !defined('TEST_SEARCH_ELASTIC_INDEX')) {
+                    $this->markTestSkipped('Elastic extension test server not set.');
+                }
+
+        set_config('hostname', TEST_SEARCH_ELASTIC_HOSTNAME, 'search_elastic');
+        set_config('port', TEST_SEARCH_ELASTIC_PORT, 'search_elastic');
+        set_config('index', TEST_SEARCH_ELASTIC_INDEX, 'search_elastic');
+
+        $this->generator = self::getDataGenerator()->get_plugin_generator('core_search');
+        $this->generator->setup();
+
+        $this->engine = new \search_elastic\testable_engine();
+        $this->search = testable_core_search::instance($this->engine);
+        $areaid = \core_search\manager::generate_areaid('core_mocksearch', 'mock_search_area');
+        $this->search->add_search_area($areaid, new core_mocksearch\search\mock_search_area());
+
+        $this->setAdminUser();
+
+        // Cleanup before doing anything on it as the index it is out of this test control.
+        $this->search->delete_index();
+
+    }
+
+    public function tearDown() {
+        // For unit tests before PHP 7, teardown is called even on skip. So only do our teardown if we did setup.
+        if ($this->generator) {
+            // Moodle DML freaks out if we don't teardown the temp table after each run.
+            $this->generator->teardown();
+            $this->generator = null;
+        }
+    }
+
+    /**
+     * Simple data provider to allow tests to be run with file indexing on and off.
+     */
+    public function file_indexing_provider() {
+        return array(
+                'file-indexing-off' => array(0)
+        );
+    }
+
+    public function test_connection() {
+        $this->assertTrue($this->engine->is_server_ready());
+    }
+
+}
