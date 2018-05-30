d365adc2812944a7137cf79a45caa3a36055d9f0
Matt Porritt <mattp@catalyst-au.net>
2018-05-30 16:38:11 +1000

    WIP: initial boilerplate to refactor file enrichment functionality into a class set

---
+++ classes/enrich/text/tika.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Document representation.
+ *
+ * @package     search_elastic
+ * @copyright   Matt Porritt <mattp@catalyst-au.net>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace search_elastic\enrich\image;
+
+defined('MOODLE_INTERNAL') || die;
+
+use search_elastic\enrich\base\base_enrich;
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Base data enrichment class.
+ *
+ * @package    search_elastic
+ * @copyright  Matt Porritt <mattp@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tika extends base_enrich {
+
+}
+
